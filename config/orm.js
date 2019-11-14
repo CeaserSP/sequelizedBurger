5f51740c5dbb07916ac1852f918a7c8f371b96db
Sheldon Perkins <csperkins18@gmail.com>
2019-11-13 20:14:03 -0500

    deleted seeds.db

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,3 @@
-var connection = require("../config/connection.js");
-
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible', true);
