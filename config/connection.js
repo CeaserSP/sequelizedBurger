66c06ad27fb9ef15fb90eea32e566e69831bf3e9
Sheldon Perkins <csperkins18@gmail.com>
2019-11-13 20:26:36 -0500

    TO DO: Figure out prelims and how to setup migration file

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("starwars", "root", "", {
+var sequelize = new Sequelize("burger", "root", "", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
